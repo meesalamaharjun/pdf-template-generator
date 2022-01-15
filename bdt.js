92b97c5bc3cb4ddb2ca5ea545ab070560cde610f
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-01-16 01:07:57 +0530

    Added all the changes to the bdt 2.0 project

--- bdt.js
+++
@@ -1,7 +0,0 @@
-const Handlebars = require("handlebars");
-const fs = require("fs");
-const path = require("path");
-const puppeteer = require("puppeteer");
-
-const sampleData=require('./data/bdt.json');
-
