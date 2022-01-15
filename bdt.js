28330f68b0f65273f4e995ea0d5eb5ab90c6c91c
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-01-16 00:11:12 +0530

    Deleted some of the files

---
+++ bdt.js
@@ -0,0 +1,7 @@
+const Handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+const puppeteer = require("puppeteer");
+
+const sampleData=require('./data/bdt.json');
+
