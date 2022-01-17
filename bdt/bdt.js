f75b907d09c84e728dca97ce5a5b3da40452621f
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-01-17 20:31:55 +0530

    Added the changes for adding the data

--- bdt/bdt.js
+++ bdt/bdt.js
@@ -1,12 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 const sampleData = require("./data/bdt.json");
+const staticData=require('./data/static.json');
 const hbs = require("hbs");
-const puppeteer=require('puppeteer');
+const puppeteer = require("puppeteer");
+const {
+  scoreCircleOffset,
+  getSubjectIcon,
+  formatDate,
+  progressBarOffset,
+  progressBarWidth,
+  progressBarGradient,
+  progressTextPosition,
+  getGradeImages,
+  getGapText,
+} = require("./utils/helpers");
 
 const main = path.resolve(__dirname + "/partials/layout.hbs");
-const pdfPath=path.join('pdf', `hello.pdf`);
-
+const pdfPath = path.join("pdf", `hello.pdf`);
 
 const registerPartials = () => {
   const partialsDir = path.resolve(__dirname + "/partials");
@@ -18,17 +29,32 @@ const registerPartials = () => {
   });
 };
 
+const registerHelpers = () => {
+  hbs.registerHelper("scoreCircleOffset", scoreCircleOffset);
+  hbs.registerHelper("getSubjectIcon", getSubjectIcon);
+  hbs.registerHelper("formatDate", formatDate);
+  hbs.registerHelper("progressBarOffset", progressBarOffset);
+  hbs.registerHelper("progressBarWidth", progressBarWidth);
+  hbs.registerHelper("progressBarGradient", progressBarGradient);
+  hbs.registerHelper("progressTextPosition", progressTextPosition);
+  hbs.registerHelper("getGradeImages", getGradeImages);
+  hbs.registerHelper("getGapText", getGapText);
+};
 
-const compileHTML = async() => {
-  const options={
-    printBackground:true,
-    path:pdfPath,
-    height:'2000'
-  }
+const compileHTML = async () => {
+  const options = {
+    printBackground: true,
+    path: pdfPath,
+  };
+  const gradeImages = getGradeImages(sampleData.grade);
+  console.log("Registering the partials");
   registerPartials();
+  console.log("Registering the helpers");
+  registerHelpers();
+  console.log("Generating the template");
   const bdtTemplate = fs.readFileSync(main, "utf-8");
   const template = hbs.handlebars.compile(bdtTemplate);
-  const html = template(sampleData);
+  const html = template({...sampleData, gradeImages,learningReport:staticData["learning-report-data"]});
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"],
     headless: true,
@@ -38,9 +64,7 @@ const compileHTML = async() => {
   await page.setContent(html);
   await page.evaluateHandle("document.fonts.ready");
   await page.pdf(options);
-  await browser.close();  
+  await browser.close();
 };
 
-
-
-module.exports=compileHTML;
+module.exports = compileHTML;
---
+++ bdt/utils/helpers.js
@@ -0,0 +1,92 @@
+const staticData = require("../data/static.json");
+
+function scoreCircleOffset(percentage) {
+  const offsetValueFromPercentage = percentage * (440 / 100);
+  return 440 - offsetValueFromPercentage;
+}
+
+function formatDate(dateString) {
+  var pr = new Intl.PluralRules("en-GB", { type: "ordinal" });
+
+  const suffixes = new Map([
+    ["one", "st"],
+    ["two", "nd"],
+    ["few", "rd"],
+    ["other", "th"],
+  ]);
+
+  const formatOrdinals = (n) => {
+    const rule = pr.select(n);
+    const suffix = suffixes.get(rule);
+    return `${n}${suffix}`;
+  };
+
+  const date = new Date(dateString);
+
+  const day = new Intl.DateTimeFormat("en-GB", {
+    day: "2-digit",
+  }).format(date);
+
+  const month = new Intl.DateTimeFormat("en-GB", {
+    month: "short",
+  }).format(date);
+
+  return `${formatOrdinals(day)} ${month}`;
+}
+
+function progressBarOffset(percentage) {
+  let parsedPercentage = parseInt(percentage, 10);
+  if (parsedPercentage === 100) return 0;
+  return parsedPercentage;
+}
+
+function progressBarWidth(percentage) {
+  let parsedPercentage = parseInt(percentage, 10);
+  if (parsedPercentage === 100) return 100;
+  return 100 - parsedPercentage;
+}
+
+function progressBarGradient(score) {
+  let parsedScore = parseInt(score, 10);
+  if (parsedScore <= 50) return "rangePoor";
+  else if (parsedScore > 50 && parsedScore <= 80) return "rangeOkay";
+  else return "rangeGood";
+}
+
+function progressTextPosition(percentage) {
+  let parsedPercentage = parseInt(percentage, 10);
+  return parsedPercentage - 8;
+}
+
+const getGradeImages = (grade) => {
+  return {
+    first: staticData.section_images[grade][0],
+    second: staticData.section_images[grade][1],
+  };
+};
+
+const getSubjectIcon = (subject) => {
+  return staticData.subject_icons[subject];
+};
+
+const getGapText = (score) => {
+  if (score > 0 && score <= 50) {
+    return "SIGNIFICANT GAP";
+  } else if (score > 50 && score <= 80) {
+    return "MODERATE GAP";
+  } else if (score > 80 && score <= 100) {
+    return "LOW GAP";
+  }
+};
+
+module.exports = {
+  scoreCircleOffset,
+  getSubjectIcon,
+  formatDate,
+  progressBarOffset,
+  progressBarWidth,
+  progressBarGradient,
+  progressTextPosition,
+  getGradeImages,
+  getGapText,
+};
