342c67c3f48cb6ea8d2facf51e5239fcc1e16007
Maharjun Meesala <meesala.maharjun@byjus.com>
2022-02-07 14:35:37 +0530

    JNT-1833 | integrated test summary page with response for k10

--- k10/k10.js
+++ k10/k10.js
@@ -4,9 +4,7 @@ const sampleData = require("./data/k10.json");
 const staticData = require("./data/static.json");
 const hbs = require("hbs");
 const puppeteer = require("puppeteer");
-const {
-  dateFormat, printValue,
-} = require("./utils/helpers");
+const { dateFormat, printValue } = require("./utils/helpers");
 
 const main = path.resolve(__dirname + "/partials/k10_layout.hbs");
 const pdfPath = path.join("pdf", `k10.pdf`);
@@ -31,7 +29,7 @@ const compileHTML = async () => {
     printBackground: true,
     path: pdfPath,
   };
-//   const gradeImages = getGradeImages(sampleData.grade);
+  //   const gradeImages = getGradeImages(sampleData.grade);
   console.log("Registering the partials");
   registerPartials();
   console.log("Registering the helpers");
@@ -39,7 +37,27 @@ const compileHTML = async () => {
   console.log("Generating the template");
   const bdtTemplate = fs.readFileSync(main, "utf-8");
   const template = hbs.handlebars.compile(bdtTemplate);
-  const html = template({...sampleData, ...staticData});
+  const subjectNames = [
+    "Mathematical Ability",
+    "Verbal Ability",
+    "Logical Ability",
+    "Environmental Science",
+    "Science",
+    "Social Science",
+  ];
+  const subjects = subjectNames.map((name) => {
+    const marksData = sampleData[name];
+    return {
+      name,
+      attempted: `${marksData.attempted} out of ${marksData.max_questions}`,
+      correct: `${marksData.correct} out of ${marksData.max_questions}`,
+      incorrect: `${marksData.incorrect} out of ${marksData.max_questions}`,
+      totalTime: `${marksData.total_time_taken}`,
+      avgTime: `${marksData.average_time_question}`,
+      percentage_faster: `${marksData.percentage_faster}% students were faster`,
+    };
+  });
+  const html = template({ ...sampleData, subjects, ...staticData });
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"],
     headless: true,
